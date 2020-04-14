e36b20c722f9ac67c62b420b36e22e73caf9cd23
Abhijeet Prasad <abhi.prasad16@gmail.com>
2020-04-14 20:28:30 +0530

    Redirect

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template
+from flask import Flask , render_template,redirect
 import requests
 
 app = Flask(__name__)   
@@ -9,6 +9,10 @@ def home():
 @app.route('/login')
 def login():
     return render_template('login.html')
+@app.route('/home')
+def redirect_home():
+    return redirect('/')
+
 
 if __name__ == '__main__':
     app.run(debug = True)
