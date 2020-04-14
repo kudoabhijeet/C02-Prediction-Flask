a58f842b4faa72c93852302b05e45adbe6d52ec4
Abhijeet Prasad <abhi.prasad16@gmail.com>
2020-04-14 20:19:54 +0530

    render_template method

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask 
+from flask import Flask , render_template   
 import requests
 
 app = Flask(__name__)   
 
 @app.route('/')
 def home():
-    return "Hello World!"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
