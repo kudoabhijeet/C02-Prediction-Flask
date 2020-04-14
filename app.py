c08add8e308087677c8d895ecbb19fec473243aa
Abhijeet Prasad <abhi.prasad16@gmail.com>
2020-04-14 20:37:19 +0530

    Ginger Template Example

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from flask import Flask , render_template,redirect
 import requests
 
 app = Flask(__name__)   
-
+fruits = ['Apple', 'Banana', 'Mango']
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', fruits = fruits)
 @app.route('/login')
 def login():
     return render_template('login.html')
