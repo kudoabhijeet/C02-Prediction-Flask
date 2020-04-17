d286781f777d3ee5be5307ea9aef38c2a43fac7a
Abhijeet Prasad <abhi.prasad16@gmail.com>
2020-04-17 16:03:29 +0530

    Loading ML Model

--- app.py
+++ app.py
@@ -1,23 +1,37 @@
 from flask import Flask , render_template,redirect,request
 import requests
+from sklearn.externals import joblib
 
 app = Flask(__name__)   
-fruits = ['Apple', 'Banana', 'Mango']
+m = joblib.load('model.pkl')
+
 @app.route('/')
 def home():
-    return render_template('index.html', fruits = fruits)
-@app.route('/login')
-def login():
-    return render_template('login.html')
-@app.route('/home')
-def redirect_home():
-    return redirect('/')
-@app.route('/submit', methods = ['POST'])
-def submit():
-    name = request.form['user']
-    f = request.files['userfile']
-    f.save(f.filename)
-    return "<h2>Hello {}".format(name)
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def model():
+    if request.method == 'POST':
+        engine_size = float(request.form['engine_size'])
+
+        emission = str(m.predict([[engine_size]]))
+        print(emission)
+    return render_template('index.html', predicted_emission = emission)
+
+# @app.route('/login')
+# def login():
+#     return render_template('login.html')
+# @app.route('/home')
+# def redirect_home():
+#     return redirect('/')
+# #@app.route('/submit', methods = ['POST'])
+# # def submit():
+# #     name = request.form['user']
+# #     f = request.files['userfile']
+# #     f.save(f.filename)
+# #     return "<h2>Hello {}".format(name)
+# @app.route('/', method='POST')
+# def load():
 
     
 if __name__ == '__main__':
